887eee37034a1f2865c0e31c0ea426299301663e
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2023-11-23 22:56:06 +0200

    The UserModelView class, responsible for presenting user-related information and interactions.

---
+++ UserModelView.h
@@ -0,0 +1,34 @@
+#ifndef USERMODELVIEW_H
+#define USERMODELVIEW_H
+
+
+#include "UserModel.h"
+
+
+class UserModelView {
+public:
+    UserModelView();
+    void Print(UserModel& user)const;
+    void listUserNameId(const UserModel& user)const;
+
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+#endif
